c336e3358e4ad7998be5fc040067db86f8fdb281
Jerome <jscar216@gmail.com>
2017-07-10 10:27:38 -0400

    Created first method add

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,6 +10,38 @@ namespace Methods
     {
         static void Main(string[] args)
         {
+            //Methods are a tool for us to create reusable pieces of code.
+
+            //Methods are a way of us collecting a series of instuctions and the call them..
+            //when we need them.
+
+            //Methods are always part of a class. Methods are always children classes.
+            //This means a method will never be  created inside another method or member of the class...
+            //However, methods are frequently called inside of another class or method.
+            Console.WriteLine("Hello. Please enter the first number to be added.");
+            int numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello. Please enter the second number to be added.");
+            int numberTwo = int.Parse(Console.ReadLine());
+
+
+            int answer = Add(numberOne, numberTwo);
+            Console.WriteLine("The sum of your two numbers are " + answer);
+
         }
+        //Method Header
+        //Access Modifier - Return Type - Method Name (in Pascal Case) - Parantheses (sometimes with parameters)
+        public static int Add(int firstnumber, int secondNumber)     //example
+        {
+            //Method Body
+            //A complete method header and body - is called a method declaration
+
+            int sum = firstnumber + secondNumber;
+
+            return sum;
+            //The "return" keyword takes whatever calue is determined by using this method...
+            //and sends that value back to where I call my method.
+        }
+        //If you have a method that does not have a return value, you would use the keyword "void"...
+        //When we have a "void" return type we dont need to use the keyword "return".
     }
 }
