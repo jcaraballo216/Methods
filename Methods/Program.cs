bf68a5fc721bf4c0a1d1f766ae2342931abf32b7
Jerome <jscar216@gmail.com>
2017-07-11 22:18:53 -0400

    More methods practice

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,145 +10,162 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            //Methods are a tool for us to create reusable pieces of code.
+            //        //Methods are a tool for us to create reusable pieces of code.
 
-            //Methods are a way of us collecting a series of instuctions and the call them..
-            //when we need them.
+            //        //Methods are a way of us collecting a series of instuctions and the call them..
+            //        //when we need them.
 
-            //Methods are always part of a class. Methods are always children classes.
-            //This means a method will never be  created inside another method or member of the class...
-            //However, methods are frequently called inside of another class or method.
+            //        //Methods are always part of a class. Methods are always children classes.
+            //        //This means a method will never be created inside another method or member of the class...
+            //        //However, methods are frequently called inside of another class or method.
 
-            //Console.WriteLine("Hello. Please enter the first number to be added.");
-            //int numberOne = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Hello. Please enter the second number to be added.");
-            //int numberTwo = int.Parse(Console.ReadLine());
-
-
-            //int answer = Add(numberOne, numberTwo);
-            //Console.WriteLine("The sum of your two numbers are " + answer);
-
-            //RobotWarning("Will Robinson");
-            //Useless();
-            //string myBirthMonth = "january";
-            //string myFriendBirthMonth = "october";
-            //string myVehicle = Vehicle(myBirthMonth);
-            //string myFriendVehicle = Vehicle(myFriendBirthMonth);
-
-            //Console.WriteLine("My future vehicle is {0} and Jordans future {1}.", myVehicle, myFriendVehicle);
-
-            //    Console.WriteLine("What is your name?");
-            //    string name = Console.ReadLine().ToUpper();
-            //    Console.WriteLine("What is your favorite food?");
-            //    string food = Console.ReadLine().ToUpper();
-            //    FavoriteFood(name, food);
-
-            //    Console.WriteLine("How old are you?");
-            //    int age = int.Parse(Console.ReadLine());
-            //    RetirementCalculator(age);
+            //        Console.WriteLine("Hello. Please enter the first number to be added.");
+            //        int numberOne = int.Parse(Console.ReadLine());
+            //        Console.WriteLine("Hello. Please enter the second number to be added.");
+            //        int numberTwo = int.Parse(Console.ReadLine());
 
 
-            //double hoursWorked = 42.3d;
-            //double hourlyWage = 12.50d;
+            //        int answer = Add(numberOne, numberTwo);
+            //        Console.WriteLine("The sum of your two numbers are " + answer);
 
-            //Console.WriteLine(WageCalculator(hoursWorked, hourlyWage));
+            //        RobotWarning("Will Robinson");
+            //        Useless();
+            //        string myBirthMonth = "january";
+            //        string myFriendBirthMonth = "october";
+            //        string myVehicle = Vehicle(myBirthMonth);
+            //        string myFriendVehicle = Vehicle(myFriendBirthMonth);
 
-            //Console.WriteLine("What is your favorite car?");
-            //string carName = Console.ReadLine().ToUpper();
-            //FavoriteCar(carName);
-            int monthDay = 1;
-            int year = 2017;
-            Console.WriteLine("Your birthday is " + (BirthMonth(monthDay, year)));
+            //        Console.WriteLine("My future vehicle is {0} and Jordans future {1}.", myVehicle, myFriendVehicle);
+
+            //        Console.WriteLine("What is your name?");
+            //        string name = Console.ReadLine().ToUpper();
+            //        Console.WriteLine("What is your favorite food?");
+            //        string food = Console.ReadLine().ToUpper();
+            //        FavoriteFood(name, food);
+
+            //        Console.WriteLine("How old are you?");
+            //        int age = int.Parse(Console.ReadLine());
+            //        RetirementCalculator(age);
+
+
+            //        double hoursWorked = 42.3d;
+            //        double hourlyWage = 12.50d;
+
+            //        Console.WriteLine(WageCalculator(hoursWorked, hourlyWage));
+
+            //        Console.WriteLine("What is your favorite car?");
+            //        string carName = Console.ReadLine().ToUpper();
+            //        FavoriteCar(carName);
+            //        int monthDay = 1;
+            //        int year = 2017;
+            //        Console.WriteLine("Your birthday is " + (BirthMonth(monthDay, year)));
+
+            //SayHello();
+
+
         }
-
-
-
-        //}
-        //Method Header
-        //Access Modifier - Return Type - Method Name (in Pascal Case) - Parantheses (sometimes with parameters)
-        //public static int Add(int firstnumber, int secondNumber)     //example
-        //{
-        //    //Method Body
-        //    //A complete method header and body - is called a method declaration
-
-        //    int sum = firstnumber + secondNumber;
-
-        //    return sum;
-            //The "return" keyword takes whatever calue is determined by using this method...
-            //and sends that value back to where I call my method.
-        //}
-        //If you have a method that does not have a return value, you would use the keyword "void"...
-        //When we have a "void" return type we dont need to use the keyword "return".
-
-       // public static void RobotWarning(string name)
-       // {
-       //     Console.WriteLine("Danger, " + name + "!!");
-       // }
-       // public static void Useless()
-       // {
-       //     Console.WriteLine("Yes Yes Yes");
-       //}
-        //public static string Vehicle(string birthMonth)
-        //{
-        //    string vehicleFortune;
-        //    if (birthMonth.ToLower() == "january" || birthMonth.ToLower() == "august")
+        //    //Method Header
+        //    //Access Modifier - Return Type - Method Name(in Pascal Case) - Parantheses(sometimes with parameters)
+        //    public static int Add(int firstnumber, int secondNumber)     //example
         //    {
-        //        vehicleFortune = "Hoverboard";
+        //        //Method Body
+        //        //A complete method header and body - is called a method declaration
+
+        //        int sum = firstnumber + secondNumber;
+
+        //        return sum;
         //    }
-        //    else
+        //    //The "return" keyword takes whatever calue is determined by using this method...
+        //    //and sends that value back to where I call my method.
+        //    //If you have a method that does not have a return value, you would use the keyword "void"...
+        //    //    When we have a "void" return type we dont need to use the keyword "return".
+
+        //    public static void RobotWarning(string name)
         //    {
-        //        vehicleFortune = "Suv";
+        //        Console.WriteLine("Danger, " + name + "!!");
         //    }
-        //    return vehicleFortune;
-  
-            
-        //}
-    //Create a method called FavoriteFood
-    //It should take two string parameters. One representing a name, and the other representing a favorite food.
-    //The return type should concatenate
+        //    public static void Useless()
+        //    {
+        //        Console.WriteLine("Yes Yes Yes");
+        //    }
+        //    public static string Vehicle(string birthMonth)
+        //    {
+        //        string vehicleFortune;
+        //        if (birthMonth.ToLower() == "january" || birthMonth.ToLower() == "august")
+        //        {
+        //            vehicleFortune = "Hoverboard";
+        //        }
+        //        else
+        //        {
+        //            vehicleFortune = "Suv";
+        //        }
+        //        return vehicleFortune;
 
-    //Create a method called RetirementCalculator
-    //It should take an int as a parameter representing the users age
-    //The method should calculate how many more years until the user retires using 65 as the age of retirement.
-    //the return type should be void
-    //Once it calculates the users retirement age it should print
-    //"The user will retire in x years" where x represents the value that was calculated
 
-    //public static void FavoriteFood(string name, string food)
-    //{
-    //    Console.WriteLine(name + " favorite food is " + food);
-    //}
-    //    public static int RetirementCalculator(int age)
-    //    {
-    //        int retirementAge = (65 - age);
-    //        Console.WriteLine("The user will retire in " + retirementAge + " years.");
-    //        return retirementAge;
-    //    }
-    ////public static double WageCalculator(double hoursWorkedWeekly , double hourlyWage)
-    ////    {
-    ////        //This method should calculate the Monthly Wage.
-    ////        double monthlyWage = (hoursWorkedWeekly * 52 * hourlyWage) / 12;
-    ////        return monthlyWage;
-    ////    }
+        //    }
+        //    //Create a method called FavoriteFood
+        //    //It should take two string parameters.One representing a name, and the other representing a favorite food.
+        //    //The return type should concatenate
 
-        //create two methods of you choosing. At least one of the methods should have a non-void return type.
-        //Both of the methods should take at least one parameter
-        //Remember when naming your method that it should represent what your method does.
-        //Your method should only aim to accomplish one thing
-        //be creative
+        //    //Create a method called RetirementCalculator
 
-        //public static void FavoriteCar(string carName)
+        //    //It should take an int as a parameter representing the users age
+
+        //    //The method should calculate how many more years until the user retires using 65 as the age of retirement.
+        //    //the return type should be void
+        //    //Once it calculates the users retirement age it should print
+        //    //    "The user will retire in x years" where x represents the value that was calculated
+
+        //    public static void FavoriteFood(string name, string food)
         //{
-        //    Console.WriteLine("Your favorite car is " + carName + ".");
+        //    Console.WriteLine(name + " favorite food is " + food);
+        //}
+        //public static int RetirementCalculator(int age)
+        //{
+        //    int retirementAge = (65 - age);
+        //    Console.WriteLine("The user will retire in " + retirementAge + " years.");
+        //    return retirementAge;
+        //}
+        //public static double WageCalculator(double hoursWorkedWeekly, double hourlyWage)
+        //{
+        //    //This method should calculate the Monthly Wage.
+        //    double monthlyWage = (hoursWorkedWeekly * 52 * hourlyWage) / 12;
+        //    return monthlyWage;
         //}
 
-        public static int BirthMonth(int monthDay, int year)
-        {
-            int birthDay = (monthDay + year);
-            return birthDay;
-        }
-            
+        ////create two methods of you choosing.At least one of the methods should have a non-void return type.
+        ////Both of the methods should take at least one parameter
 
-        
-    }
-}
+
+
+
+        ////Remember when naming your method that it should represent what your method does.
+        ////Your method should only aim to accomplish one thing
+        ////be creative
+
+        //        public static void FavoriteCar(string carName)
+        //    {
+        //        Console.WriteLine("Your favorite car is " + carName + ".");
+        //    }
+
+        //    public static int BirthMonth(int monthDay, int year)
+        //    {
+        //        int birthDay = (monthDay + year);
+        //        return birthDay;
+        //    }
+
+        //public static void SayHello()
+        //{
+        //    Console.WriteLine("Say Hello");
+        //}
+
+
+
+
+
+          }
+
+      }
+
+
+
